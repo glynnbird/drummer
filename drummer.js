6fe7c017135ab487cd4a672c2a95aa20ca360009
Glynn Bird <glynn.bird@gmail.com>
2017-08-31 11:08:46 +0100

    tidy up

--- drummer.js
+++ drummer.js
@@ -1,17 +1,16 @@
-
 const SEQUENCE_LENGTH = 32;
 
+// create an empty sequence
 var emptyArray = function() {
   var arr = [];
   for (var i = 0; i < SEQUENCE_LENGTH; i++) {
-    arr.push(0);
+    arr.push(false);
   }
   return arr;
 };
 
 // setup the database
 var db = new PouchDB('drummer');
-
 
 var app = new Vue({
   el: '#app',
@@ -25,23 +24,36 @@ var app = new Vue({
     interval: null,
     bpm: 120,
     name: '',
-    files: [ 'a','b','c']
+    files: [ ]
   },
   methods: {
+    // start the sequence playback
     start: function() {
-      this.paused = false;
-      var t = 60000 / (this.bpm * 4);
+
+
       if (this.interval === null) {
+        this.paused = false;
+        
+        // calculate time interval from bpm
+        var t = 60000 / (this.bpm * 4);
+
+        // start the timer
         this.interval = setInterval(() => {
+
+          // on each tick, play the sound if necessary
           for (var j in this.sequence) {
             if (this.sequence[j][this.pos] && !this.sounds[j].mute) {
               this.sounds[j].sound.play();
             }
           }
+          
+          // move the position along and wrap around
           this.pos = (this.pos +1) % this.sequenceLength;
         }, t);
       }
     },
+
+    // stop playback
     stop: function() {
       if (this.interval !== null) {
         clearInterval(this.interval);
@@ -49,6 +61,8 @@ var app = new Vue({
         this.paused = true;
       }
     },
+
+    // retrieve a list of documents from the database
     getFileList: function() {
       this.files = [];
       db.find({ selector: {}, fields: ['_id']}).then((data) => {
@@ -59,45 +73,65 @@ var app = new Vue({
         }
       });
     },
+
+    // on click of the play button
     onPlay : function() {
       this.start();
     },
+
+    // on click of the stop button
     onStop: function() {
       this.stop();
     },
+
+    // on click of the clear button
     onClear: function() {
       for (var j in this.sequence) {
-        this.sequence[j] = emptyArray();
+        for(var i in this.sequence[j]) {
+          this.sequence[j][i] = false;
+        }
       }
     },
+
+    // on click of the bpm button
     onBpm: function() {
       this.stop();
       this.start();
     },
+
+    // on click of the sequence length buttons
     onSeqLen: function() {
       this.stop();
       this.start();
     },
+
+    // on click of the load button
     onLoad: function() {
       $('#loadModal').modal('show')
     },
+
+    // on click of the save button
     onSave: function() {
-      var obj = {
-        _id: this.name,
-        sequenceLength: this.sequenceLength,
-        sequence: this.sequence,
-        bpm: this.bpm,
-        name: this.name
-      };
-      db.get(obj._id).then((data) => {
-        obj._rev = data._rev;
-        return db.put(obj);
-      }).catch(() => {
-        return db.put(obj);
-      }).then(() => {
-        this.getFileList();
-      });
+      if (this.name) {
+        var obj = {
+          _id: this.name,
+          sequenceLength: this.sequenceLength,
+          sequence: this.sequence,
+          bpm: this.bpm,
+          name: this.name
+        };
+        db.get(obj._id).then((data) => {
+          obj._rev = data._rev;
+          return db.put(obj);
+        }).catch(() => {
+          return db.put(obj);
+        }).then(() => {
+          this.getFileList();
+        });
+      }
     },
+
+    // when the user chooses to load a sequence from the database
     onModalLoad: function() {
       db.get(this.name).then((data) => {
         this.name = data.name;
@@ -109,13 +143,17 @@ var app = new Vue({
         $('#loadModal').modal('hide')
       });
     },
+
+    // on click of the 'get started' button
     onGetStarted : function() {
       $('#startupModal').modal('hide')
     }
   },
-  created: function() {
-    console.log('here');
 
+  // app start up event
+  created: function() {
+
+    // load the sounds
     this.sounds = {
       kicka: { mute: false, sound: new Howl({ src: ['samples/Kick.wav']})},
       kickb: { mute: false, sound:new Howl({ src: ['samples/Kick Accent.wav']})},
@@ -134,28 +172,17 @@ var app = new Vue({
       tamb2: { mute: false, sound:new Howl({ src: ['samples/Tamb 2.wav']})}
     };
 
+    // clear the sequence
     for (var i in this.sounds) {
       this.sequence[i] = emptyArray();
     }
+
+    // simple back beat
     for (var x=0; x < this.sequenceLength; x+=4) {
-      this.sequence.kicka[x] = 1;
+      this.sequence.kicka[x] = true;
     }
     this.getFileList();
     this.start();
     $('#startupModal').modal('show')
   }
 })
-
-/*
-var i = 0;
-setInterval(function() {
-  //console.log(app.sequence.kick);
-  i = app.pos;
-  for (var j in app.sequence) {
-    if (app.sequence[j][i] && !app.sounds[j].mute) {
-      app.sounds[j].sound.play();
-    }
-  }
-  i++;
-  i = i % SEQUENCE_LENGTH;
-}, 125);*/
